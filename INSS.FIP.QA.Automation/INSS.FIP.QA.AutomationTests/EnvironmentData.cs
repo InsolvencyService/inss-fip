c4d55174170d38740ed4c6263267b60dd66799cb
seeswarawaka <seeswarawaka@nomensa.com>
2022-10-10 17:46:03 +0100

    updated test automation framework

---
+++ INSS.FIP.QA.Automation/INSS.FIP.QA.AutomationTests/EnvironmentData.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSS.FIP.QA.AutomationTests.Utils
+{
+    public static class EnvironmentData
+    {
+        public static string baseUrl { get; } = TestContext.Parameters["baseUrl"];
+    }
+}
---
+++ INSS.FIP.QA.Automation/INSS.FIP.QA.AutomationTests/Features/VerifyFIPHomePage.feature
@@ -0,0 +1,5 @@
+﻿Feature: Verify FIP Home Page
+
+@Regression
+Scenario: Verify User is able to navigate to FIP website
+	Given I navigate to FIP page
