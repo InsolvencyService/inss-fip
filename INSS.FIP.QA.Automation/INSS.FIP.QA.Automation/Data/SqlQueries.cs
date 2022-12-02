a2a904e08801320182e5eac51ab3a7cc435b7493
NomensaMayur <mrana@nomensa.com>
2022-12-02 13:00:11 +0000

    Initial Commit

---
+++ INSS.FIP.QA.Automation/INSS.FIP.QA.Automation/Data/SqlQueries.cs
@@ -0,0 +1,17 @@
+﻿using INSS.FIP.QA.Automation.TestFramework;
+using INSS.FIP.QA.Automation.Helpers;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace INSS.FIP.QA.Automation.Data
+{
+    public class SqlQueries
+    {
+        private static readonly string ConnectionString = WebDriverFactory.Config["DBConnectionString"];
+
+    
+       
+    }
+}
