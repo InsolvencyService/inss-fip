ba37ae51f563d43b012edbaa2c80e2d7a9350525
PRABHAT SHUKLA <Prabhat.Shukla@insolvency.gov.uk>
2025-02-11 16:39:05 +0000

    Added both heet and timer trigger

--- INSS.FIP.Functions/Functions/DBSynchFCMCToInsight/DBSynchGetTimerTrigger.cs
+++ INSS.FIP.Functions/Functions/DBSynchFCMCToInsight/DBSynchGetTimerTrigger.cs
@@ -1,12 +1,14 @@
 ﻿using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
 using INSS.FIP.Data;
 using INSS.FIP.Data.FCMCDataSource;
 using INSS.FIP.Functions.Functions.InsolvencyPractitioner;
+//using INSS.FIP.Functions.Functions.InsolvencyPractitioner;
 using INSS.FIP.Models.ResponseModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.WebJobs;
+//using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
@@ -19,15 +21,15 @@ using TimerTriggerAttribute = Microsoft.Azure.Functions.Worker.TimerTriggerAttri
 
 namespace INSS.FIP.Functions.Functions.DBSynchFCMCToInsight;
 
-public class DBSynchGetHttpTrigger
+public class DBSynchGetTimerTrigger
 {
-    private readonly ILogger<DBSynchGetHttpTrigger> _logger;
+    private readonly ILogger<DBSynchGetTimerTrigger> _logger;
     private readonly IMapper _mapper;
     private readonly iirwebdbContext _iirwebdbContext;
     private readonly SourceDbContext _sourceDbContext;
 
-    public DBSynchGetHttpTrigger(
-       ILogger<DBSynchGetHttpTrigger> logger,
+    public DBSynchGetTimerTrigger(
+       ILogger<DBSynchGetTimerTrigger> logger,
        IMapper mapper,
        iirwebdbContext iirwebdbContext,
        SourceDbContext sourceDbContext)
@@ -38,16 +40,19 @@ public class DBSynchGetHttpTrigger
         _sourceDbContext = sourceDbContext;
     }
 
-    [Function("DBSynchHttp")]
-    [OpenApiOperation(operationId: "DBSynchHttp", tags: new[] { "DBSynchHttp" }, Summary = "Transfer data from fcmc to Insight.", Description = "Transfer data from fcmc to Insight.", Visibility = OpenApiVisibilityType.Important)]
+
+    [Function("DBSynch")]
+    [OpenApiOperation(operationId: "DBSynch", tags: new[] { "DBSynch" }, Summary = "Transfer data from fcmc to Insight.", Description = "Transfer data from fcmc to Insight.", Visibility = OpenApiVisibilityType.Important)]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: MediaTypeNames.Application.Json, bodyType: typeof(IList<FipApiSearchResultResponseModel>), Summary = "DB Synch", Description = "Transfer data from fcmc to Insight.")]
     [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid request/validation failures", Description = "Invalid request/validation failures")]
     [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.InternalServerError, Summary = "Error processing request", Description = "Error processing request")]
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required for HttpTrigger signature")]
-    public void  Run([Microsoft.Azure.Functions.Worker.HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "DBSynchHttp")] HttpRequest req)
+    public void Run([TimerTrigger("%CronPattern%")] Microsoft.Azure.Functions.Worker.TimerInfo myTimer)
+    //[HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "DBSynch")] HttpRequest req)
     {
         DBSynch();
     }
+
     private void DBSynch()
     {
         Console.WriteLine("DBSynch Triger start");
@@ -63,7 +68,7 @@ public class DBSynchGetHttpTrigger
         {
             try
             {
-                var viewData = await _sourceDbContext.vw_FindIps.ToListAsync();
+                var viewData = await _sourceDbContext.vw_FindIps.ToListAsync(CancellationToken.None);
 
                 var uniqueViewData = viewData
                 .GroupBy(x => x.IpNo)
@@ -115,7 +120,7 @@ public class DBSynchGetHttpTrigger
                     if (item.AuthBodyCode == null) // Check for nulls
                     {
                         // Handle or log the null case
-                        item.AuthBodyCode = "null"+i;
+                        item.AuthBodyCode = "null" + i;
                         _logger.LogTrace("Mapped data contains a record with NULL AuthBodyCode");
                         i++;
                     }
