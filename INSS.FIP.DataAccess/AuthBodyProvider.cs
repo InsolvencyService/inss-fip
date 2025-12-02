52464fe5c9b2f640129666dd2bbf4bc24886c5f4
John Macdonald <john.macdonald@insolvency.gov.uk>
2025-12-02 17:39:19 +0000

    APP-6924 Enablen INSSight data for ../authbody page

--- INSS.FIP.DataAccess/AuthBodyProvider.cs
+++ INSS.FIP.DataAccess/AuthBodyProvider.cs
@@ -3,6 +3,8 @@ using AutoMapper;
 using INSS.FIP.Data;
 using INSS.FIP.Interfaces;
 using INSS.FIP.Models.ResponseModels;
+using Microsoft.Extensions.Configuration;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace INSS.FIP.DataAccess;
 
@@ -11,13 +13,16 @@ public class AuthBodyProvider : IAuthBodyProvider
 {
     private readonly IMapper _mapper;
     private readonly iirwebdbContext _iirwebdbContext;
+    private readonly IConfiguration _configuration;
 
     public AuthBodyProvider(
         IMapper mapper,
-        iirwebdbContext iirwebdbContext)
+        iirwebdbContext iirwebdbContext,
+        IConfiguration configuration)
     {
         _mapper = mapper;
         _iirwebdbContext = iirwebdbContext;
+        _configuration = configuration;
     }
 
     private IQueryable<CiIpAuthorisingBody> BaseQuery
@@ -31,14 +36,35 @@ public class AuthBodyProvider : IAuthBodyProvider
         }
     }
 
+    private IQueryable<Data.FCMCDataSource.FindIpAuthBody> BaseQueryFCMCView
+    {
+        get
+        {
+            var query = from a in _iirwebdbContext.FindIpAuthBodies
+                        select a;
+
+            return query;
+        }
+    }
+
     public async Task<IList<FipApiAuthBodyResponseModel>> GetAsync()
     {
-        var query = BaseQuery;
+        var results = new List<FipApiAuthBodyResponseModel>();
 
-        var results = (from a in query
+        if (Convert.ToBoolean(_configuration["usingFCMCtableView"]))
+        {
+            results = (from a in BaseQueryFCMCView
                        orderby a.AuthBodyName
                        select _mapper.Map<FipApiAuthBodyResponseModel>(a)
-                      ).ToList();
+              ).ToList();
+        }
+        else 
+        {
+            results = (from a in BaseQuery
+                       orderby a.AuthBodyName
+                       select _mapper.Map<FipApiAuthBodyResponseModel>(a)
+              ).ToList();
+        }
 
         return await Task.FromResult(results);
     }
---
+++ INSS.FIP.Functions.UnitTests/ProviderTests/AuthBodyProviderTests.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using INSS.FIP.Data;
+using INSS.FIP.DataAccess;
+using INSS.FIP.Functions.AutoMapperProfiles;
+using INSS.FIP.Interfaces;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FakeItEasy;
+using Xunit;
+using INSS.FIP.Functions.Functions.AuthBody;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Azure.Identity;
+using System.Drawing;
+using System.Configuration;
+
+namespace INSS.FIP.Functions.UnitTests.ProviderTests
+{
+    public class AuthBodyProviderTests
+    {
+
+        private IMapper _mapper;
+        private readonly IConfiguration _fakeConfig = A.Fake<IConfiguration>();
+
+        [Theory]
+        [InlineData("true", 2)]
+        [InlineData("false", 0)]
+        [InlineData(null, 0)]
+        public void UseINSSigthDataTest(string? configFlagValue, int recordCount)
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<iirwebdbContext>()
+                .UseInMemoryDatabase(databaseName: "iirDatabase")
+                .Options;
+
+            // Insert seed data into the database using one instance of the context
+            using (var context = new iirwebdbContext(options))
+            {
+                context.FindIpAuthBodies.Add(new Data.FCMCDataSource.FindIpAuthBody { AuthBodyCode = "1", AuthBodyName = "John" });
+                context.FindIpAuthBodies.Add(new Data.FCMCDataSource.FindIpAuthBody { AuthBodyCode = "2", AuthBodyName = "Peter" });
+                //You cannot 'Add' records to EF table which has no primary key, context.Database.ExecuteSQLRaw can be used but not on an InMemoryDatabase 
+                //context.CiIpAuthorisingBodies.Add(new CiIpAuthorisingBody { AuthBodyCode = "3", AuthBodyName = "Mary" });   
+                context.SaveChanges();
+
+            }
+
+            MapperConfiguration mapperConfig = new(
+                 cfg =>
+                 {
+                     cfg.AddProfile(new EntityFrameworkToApiModelProfiles());
+                 });
+
+            _mapper = new Mapper(mapperConfig);
+
+            A.CallTo(() => _fakeConfig["usingFCMCtableView"]).Returns(configFlagValue);
+
+            using (var context = new iirwebdbContext(options))
+            {
+                IAuthBodyProvider authProvider = new AuthBodyProvider(_mapper, context, _fakeConfig);
+
+                //Act   
+                var authRecords = authProvider.GetAsync().Result;
+
+                //Assert
+                Assert.Equal(recordCount, authRecords.Count);
+
+                //Clean up InMemory database
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
