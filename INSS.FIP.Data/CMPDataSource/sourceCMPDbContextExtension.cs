7ac1ac9fd4822c71f2e7a8211baa19f9b84a8a9a
John Macdonald <john.macdonald@insolvency.gov.uk>
2026-02-20 17:11:20 +0000

    APP-6286 Check the configured view has all the required columns

---
+++ INSS.FIP.Data/CMPDataSource/sourceCMPDbContextExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INSS.FIP.Data.CMPDataSource
+{
+    public static class sourceCMPDbContextExtension
+    {
+        public static string GetViewNameWithSchema<T>(this sourceCMPDbContext context) where T : class
+        {
+            var entityType = context.Model.FindEntityType(typeof(T));
+            var qualifiedViewName = entityType?.GetSchemaQualifiedViewName();
+            return qualifiedViewName ?? "Unknown (Check configured values for CMPDataViewNameSchema and CMPDataViewName)";
+        }
+    }
+}
